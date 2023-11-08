dff6df16a915607e72ebbdadfab3ea781753fb7c
mrtng1 <mrtngrulyo@gmail.com>
2023-11-09 00:57:45 +0100

    Frontend update

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -2,13 +2,23 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../Environments/environment";
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  burgers: any[] = []; // Replace 'any' with an interface or class for the burger data
+  burgers: any[] = [];
+  imageExtensions: { [key: string]: string } = {
+    'Cheeseburger': 'jpg',
+    'Truffleburger': 'png',
+    'hot-bird': 'JPG',
+    'monster-burger': 'JPG',
+    'kids-burger': 'JPG',
+    'bernaise-burger': 'JPG',
+    'cherry-burger': 'JPG',
+  };
 
   constructor(private http: HttpClient) {
   }
@@ -16,11 +26,22 @@ export class MainComponent implements OnInit {
   ngOnInit() {
     this.http.get<any[]>(`${environment.baseUrl}/api/burgers`).subscribe({
       next: (data) => {
-        this.burgers = data;
+        this.burgers = data.map(burger => ({
+          ...burger,
+          imageUrl: this.getImageUrl(burger.burgerName)
+        }));
       },
       error: (error) => {
         console.error('Error fetching burgers:', error);
       }
     });
   }
+
+  getImageUrl(burgerName: string): string {
+    const formattedName = burgerName.toLowerCase().replace(/\s+/g, '-');
+    const extension = this.imageExtensions[burgerName] || 'JPG';
+    return `/assets/burgers/${formattedName}.${extension}`;
+  }
+
+
 }
