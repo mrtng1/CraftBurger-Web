5910ad7cc020186ca1c1914bbdad1619eaa2cc22
Tomáš Dračka <tomidracka@gmail.com>
2023-11-14 11:08:59 +0100

    Menu Component

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,79 +1,70 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {environment} from "../../Environments/environment";
-import {Router} from "@angular/router";
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { BurgerService } from "../burger.service";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
-  burgers: any[] = [];
-  currentIndex: number = 0;
+    burgers: any[] = [];
+    currentIndex: number = 0;
 
-  constructor(private router: Router, private http: HttpClient) {
-  }
+    constructor(
+        private router: Router,
+        private http: HttpClient,
+        private burgerService: BurgerService
+    ) {}
 
-  ngOnInit() {
-    this.http.get<any[]>(`${environment.baseUrl}/api/burgers`).subscribe({
-      next: (data) => {
-        this.burgers = data.map(burger => ({
-          ...burger,
-          imageUrl: this.getImageUrl(burger.burgerName)
-        }));
-      },
-      error: (error) => {
-        console.error('Error fetching burgers:', error);
-      }
-    });
-  }
-
-  getBurgerDetails(burgerId: number) {
-    this.router.navigate(['/inspect', burgerId]);
-  }
-
-  getImageUrl(burgerName: string): string {
-    const formattedName = burgerName.toLowerCase().replace(/\s+/g, '-');
-    return `/assets/burgers/${formattedName}.JPG`;
-  }
-
-  nextBurger() {
-    this.currentIndex = (this.currentIndex + 1) % this.burgers.length;
-  }
-
-  previousBurger() {
-    this.currentIndex =
-      (this.currentIndex - 1 + this.burgers.length) % this.burgers.length;
-  }
-
-  get previousIndex(): number {
-    return this.currentIndex === 0 ? this.burgers.length - 1 : this.currentIndex - 1;
-  }
-
-  get nextIndex(): number {
-    return this.currentIndex === this.burgers.length - 1 ? 0 : this.currentIndex + 1;
-  }
-
-  addToCart(burger: any) {
-    let cart = sessionStorage.getItem('cart');
-    let cartArray;
-
-    if (cart) {
-      cartArray = JSON.parse(cart);
-    } else {
-      cartArray = [];
+    ngOnInit() {
+        this.burgerService.getBurgers().subscribe({
+            next: (data) => {
+                this.burgers = data.map(burger => ({
+                    ...burger,
+                    imageUrl: this.burgerService.getImageUrl(burger.burgerName)
+                }));
+            },
+            error: (error) => {
+                console.error('Error fetching burgers:', error);
+            }
+        });
     }
 
-    cartArray.push(burger);
-    sessionStorage.setItem('cart', JSON.stringify(cartArray));
-  }
-
-  scrollToBurgerGrid() {
-    const grid = document.getElementById('burgerGrid');
-    if (grid) {
-      grid.scrollIntoView({behavior: 'smooth'});
+    redirectToMenu() {
+        this.router.navigate(['/menu']);
     }
-  }
+
+    nextBurger() {
+        this.currentIndex = (this.currentIndex + 1) % this.burgers.length;
+    }
+
+    previousBurger() {
+        this.currentIndex =
+            (this.currentIndex - 1 + this.burgers.length) % this.burgers.length;
+    }
+
+    get previousIndex(): number {
+        return this.currentIndex === 0 ? this.burgers.length - 1 : this.currentIndex - 1;
+    }
+
+    get nextIndex(): number {
+        return this.currentIndex === this.burgers.length - 1 ? 0 : this.currentIndex + 1;
+    }
+
+    addToCart(burger: any) {
+        let cart = sessionStorage.getItem('cart');
+        let cartArray;
+
+        if (cart) {
+            cartArray = JSON.parse(cart);
+        } else {
+            cartArray = [];
+        }
+
+        cartArray.push(burger);
+        sessionStorage.setItem('cart', JSON.stringify(cartArray));
+    }
 }
--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {BurgerService} from "../burger.service";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  burgers: any[] = [];
+  currentIndex: number = 0;
 
+  constructor(private burgerService: BurgerService) {}
+
+  ngOnInit() {
+    this.burgerService.getBurgers().subscribe({
+      next: (data) => {
+        this.burgers = data.map(burger => ({
+          ...burger,
+          imageUrl: this.burgerService.getImageUrl(burger.burgerName)
+        }));
+      },
+      error: (error) => {
+        console.error('Error fetching burgers:', error);
+      }
+    });
+  }
+
+  getBurgerDetails(burgerId: number) {
+    this.burgerService.getBurgerDetails(burgerId);
+  }
+
+  addToCart(burger: any) {
+    this.burgerService.addToCart(burger);
+  }
+
+  getImageUrl(burgerName: string): string {
+    return this.burgerService.getImageUrl(burgerName);
+  }
 }
