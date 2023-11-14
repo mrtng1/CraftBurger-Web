dc61f2c71eac141deb9eeb967386d3c0a1ab5074
mrtng1 <mrtngrulyo@gmail.com>
2023-11-14 14:02:54 +0100

    about us button improvements

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -9,11 +10,19 @@ export class HeaderComponent {
   isCartVisible = false;
   @Output() aboutUsClick = new EventEmitter<void>();
 
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
   toggleCart() {
     this.isCartVisible = !this.isCartVisible;
   }
 
   aboutUsClicked() {
-    this.aboutUsClick.emit();
+    if (this.route.snapshot.routeConfig?.path === 'home') {
+      this.aboutUsClick.emit();
+    } else {
+      this.router.navigate(['/home']).then(() => {
+        this.aboutUsClick.emit();
+      });
+    }
   }
 }
