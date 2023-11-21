6f8e6413cb4a7afed20ae2104224861c5a138b1c
Tomáš Dračka <tomidracka@gmail.com>
2023-11-21 16:39:48 +0100

    Cart items status - static

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
@@ -6,11 +6,20 @@ import {ActivatedRoute, Router} from "@angular/router";
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  isCartVisible = false;
+export class HeaderComponent implements OnInit{
   @Output() aboutUsClick = new EventEmitter<void>();
+  cartCount: number = 0;
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.updateCartCount();
+  }
+
+  updateCartCount() {
+    const cart = sessionStorage.getItem('cart');
+    this.cartCount = cart ? JSON.parse(cart).length : 0;
+  }
 
   aboutUsClicked() {
     if (this.route.snapshot.routeConfig?.path === 'home') {
