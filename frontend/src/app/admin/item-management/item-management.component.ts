e96522eea2d52f4ec2963f44433df781945273b9
mrtng1 <mrtngrulyo@gmail.com>
2023-11-22 13:27:18 +0100

    table in item-management

--- frontend/src/app/admin/item-management/item-management.component.ts
+++ frontend/src/app/admin/item-management/item-management.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HttpClient} from "@angular/common/http";
+import {MenuItem} from "../../models/MenuItem";
+import { BurgerService } from '../../service/burger.service';
+import { FriesService } from '../../service/fries.service';
 
 @Component({
   selector: 'app-item-management',
@@ -9,6 +13,36 @@ import { CommonModule } from '@angular/common';
   styleUrl: './item-management.component.css'
 })
 export class ItemManagementComponent {
+  menuItems: MenuItem[] = [];
+
+  constructor(private burgerService: BurgerService,
+              private friesService: FriesService) {}
+
+  ngOnInit() {
+    this.fetchMenuItems();
+  }
+
+  fetchMenuItems(): void {
+    this.burgerService.getBurgers().subscribe(burgers => {
+      const burgerItems: MenuItem[] = burgers.map(burger => ({
+        id: burger.id, // Ensure the property names match
+        name: burger.burgerName,
+        price: burger.burgerPrice,
+        type: 'Burger'
+      }));
+      this.menuItems = [...this.menuItems, ...burgerItems];
+    });
+
+    this.friesService.getFries().subscribe(fries => {
+      const friesItems: MenuItem[] = fries.map(fry => ({
+        id: fry.id, // Ensure the property names match
+        name: fry.friesName,
+        price: fry.friesPrice,
+        type: 'Fries'
+      }));
+      this.menuItems = [...this.menuItems, ...friesItems];
+    });
+  }
   createItem(): void {
     console.log("Create Item clicked");
   }
