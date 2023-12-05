162841a09cd8b974a93db09916e9be75ff13cb63
Tomáš Dračka <tomidracka@gmail.com>
2023-12-05 10:28:12 +0100

    dip.service

--- frontend/src/service/dip.service.ts
+++ frontend/src/service/dip.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
-import { environment } from "../../Environments/environment";
+import {environment} from "../Environments/environment";
 
 @Injectable({
   providedIn: 'root'
