3a043c2ae8a64a7fa1771a6b12dbace430230c1d
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2023-12-06 12:48:46 +0100

    auth guard fix

--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -1,23 +1,34 @@
 import {CanActivate, CanActivateFn, Router} from '@angular/router';
 import {Injectable} from "@angular/core";
+import {map} from "rxjs";
+import {environment} from "../Environments/environment";
+import {HttpClient} from "@angular/common/http";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   canActivate(): boolean {
     const token = localStorage.getItem('SessionToken');
 
-    if (token) {
-      // Optionally, you can add more logic here to validate the token.
-      return true;
-    } else {
+    if (!token) {
       this.router.navigate(['/login']);
       return false;
     }
+
+    // Call backend to validate the token
+    this.http.post<boolean>(`${environment.baseUrl}/Auth/validateToken`, { Token: token })
+      .pipe(map(isValid => {
+        if (!isValid) {
+          this.router.navigate(['/login']);
+        }
+        return isValid;
+      })).subscribe();
+
+    // This might need refinement for asynchronous handling
+    return true;
   }
 }
