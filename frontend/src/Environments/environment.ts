9e36ea02c8ae7a6692d83c859f4ef79c4119bd98
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2023-12-15 16:28:43 +0100

    url fix

--- frontend/src/Environments/environment.ts
+++ frontend/src/Environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "http://craftburgerweb.azurewebsites.net"
+  baseUrl: "https://craftburgerweb.azurewebsites.net"
   //baseUrl: "http://localhost:5113"
 };
