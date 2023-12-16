5b50bf47362acdcf8b6e55a3d77f91013a025e40
Tomáš Dračka <tomidracka@gmail.com>
2023-12-16 23:15:03 +0100

    workflows

--- backend/test/Helper.cs
+++ backend/test/Helper.cs
@@ -22,7 +22,7 @@ public static class Helper
         {
             var uri = new Uri(rawConnectionString);
             var properlyFormattedConnectionString = string.Format(
-                "Server={0};Database={1};User Id={2};Password={3};Port={4};Pooling=true;MaxPoolSize=1;",
+                "Server={0};Database={1};User Id={2};Password={3};Port={4};Pooling=true;MaxPoolSize=3;",
                 uri.Host,
                 uri.AbsolutePath.Trim('/'),
                 uri.UserInfo.Split(':')[0],
